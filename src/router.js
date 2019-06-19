3b0d25f437dbecf6a888e8ce8dda56afd1dc73b9
decadehew <yong_323@hotmail.com>
2019-06-19 09:56:53 +0800

    init

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Layout from './views/layout'
 
 Vue.use(Router)
 
@@ -8,11 +8,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/about',
       name: 'about',
---
+++ src/views/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nav />
+  </div>
+</template>
+<script>
+import Nav from './components/Nav'
+export default {
+  name: 'Layout',
+  components: {
+    Nav
+  }
+}
+</script>
