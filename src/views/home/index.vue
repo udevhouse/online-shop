4c9a977c39b213c6776126ae7878b7ecf5b8cb9a
leowang <rfv7855659@gmail.com>
2019-06-22 18:32:43 +0800

    完成首頁操作card

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
-    leo
+    <operate></operate>
   </div>
 </template>
 
 <script>
-
+import Operate from '@/views/home/components/Operate'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Operate
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .home{
+    padding: 20px 150px;
+  }
+</style>
