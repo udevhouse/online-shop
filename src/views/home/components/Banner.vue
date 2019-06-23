b9d4ef01baae7709f7db7a36fed7770e9f8a94a0
leowang <rfv7855659@gmail.com>
2019-06-23 15:24:09 +0800

    完成首頁banner切版

---
+++ src/views/home/components/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="banner">
+        <div class="banner__item" v-for="item of 6" :key="item">
+            <el-image class="banner__item--img" src="https://s3-ap-northeast-1.amazonaws.com/nidin-production/banner/b_2_h_b_w_20190617_133852_8d427.jpg"></el-image>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home-banner'
+}
+</script>
+
+<style lang="scss" scoped>
+    .banner{
+        &__item{
+            margin-bottom: 10px;
+            &--img{
+                width: 100%;
+                height: 90px;
+            }
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="home">
     <operate></operate>
+    <div class="home__wrap">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <banner></banner>
+        </el-col>
+        <el-col :span="12">
+          123
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 import Operate from '@/views/home/components/Operate'
+import Banner from '@/views/home/components/Banner'
+
 export default {
   name: 'home',
   components: {
-    Operate
+    Operate,
+    Banner
   }
 }
 </script>
@@ -17,5 +30,8 @@ export default {
 <style lang="scss" scoped>
   .home{
     padding: 20px 150px;
+    &__wrap{
+      margin:20px 0;
+    }
   }
 </style>
