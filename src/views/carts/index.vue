9aeb340d41dc7dce5871030ea9fe519756b0f91c
decadehew <yong_323@hotmail.com>
2019-06-25 22:06:21 +0800

    加強購物車UI
    1. 新增mock data來顯示products資料

--- src/views/carts/index.vue
+++ src/views/carts/index.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    <!-- <empty-product></empty-product> -->
-    <products></products>
+    <products
+      v-if="showProduct"
+      :productsData="productsData"></products>
+    <empty-product v-else></empty-product>
   </div>
 </template>
 <script>
 import EmptyProduct from '@/views/carts/components/EmptyProduct'
 import Products from '@/views/carts/components/Products'
+import d from '../../mocks/products.json'
 
 export default {
   name: 'Cart',
   components: {
     EmptyProduct,
     Products
+  },
+  data () {
+    return {
+      productsData: []
+    }
+  },
+  created () {
+    this.productsData = d
+  },
+  computed: {
+    showProduct () {
+      return this.productsData.length > 0
+    }
   }
 }
 </script>
